1deb821c04ac809aa309dd6f0cc2e796aa51aef0
mightnent <mightnent@gmail.com>
2021-01-14 11:17:46 +0800

    replaceChain

--- blockchain.js
+++ blockchain.js
@@ -30,6 +30,19 @@ class Blockchain{
         }
         return true;
     }
+
+    replaceChain(chain){
+
+        if(chain.length<=this.chain.length){
+            return;
+        }
+
+        if(!Blockchain.isValidChain(chain)){
+            return ;
+        }
+
+        this.chain = chain;
+    }
 }
 
 module.exports = Blockchain;
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,12 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('blockchain', () => {
-    let blockchain;
+    let blockchain, newChain, originalChain;
 
     beforeEach(() => {
         blockchain = new Blockchain();
+        newChain = new Blockchain();
+        originalChain = blockchain;
     });
 
     it('is an array', () => {
@@ -51,7 +53,7 @@ describe('blockchain', () => {
 
             describe('when the chain contain block with invalid field', () => {
                 it('returns false',()=>{
-                    console.log(blockchain.chain);
+                    
                     //wrong
                     blockchain.chain[2].data = 'banaana';
                     //expect
@@ -61,12 +63,45 @@ describe('blockchain', () => {
 
             describe('the chain does not have any invalid elements', () => {
                 it('returns true',()=>{
-                    console.log(blockchain.chain[1].data);
+                    
                     expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
                     
                 });
             });
         });
 
+    });
+
+    describe('replaceChain', () => {
+        describe('when new chain is not longer',()=>{
+            it('should not replace chain',()=>{
+                //element zero is genesis block, which is an obj
+                newChain.chain[0] = {new:'random'};//this is random key val pair
+                blockchain.replaceChain(newChain.chain);
+                expect(blockchain.chain).toEqual(originalChain.chain);
+            });
+        });
+
+        describe('when new chain is longer', () => {
+            beforeEach(() => {
+                newChain.addBlock({newData:'apple'});
+                newChain.addBlock({newData:'cidar'});
+                newChain.addBlock({newData:'banana'});
+            });
+            describe('and chain is invalid', () => {
+                it('should not replace chain',()=>{
+                    newChain.chain[2].lastHash = 'tempered';
+                    blockchain.replaceChain(newChain.chain);
+                    expect(blockchain.chain).toEqual(originalChain.chain);
+                });
+            });
+
+            describe('and chain is valid', () => {
+                it('should replace chain',()=>{
+                    blockchain.replaceChain(newChain.chain);
+                    expect(blockchain.chain).toEqual(newChain.chain);
+                });
+            });
+        });
     });
 });
