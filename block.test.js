4b428999448c336d3549b048e41ea7780727c5b6
mightnent <mightnent@gmail.com>
2021-01-18 14:39:06 +0800

    difficulty and nonce

--- block.test.js
+++ block.test.js
@@ -1,18 +1,23 @@
 const Block = require("./block");
 const {GENESIS_DATA} = require("./config");
+const cryptoHash = require("./crypto-hash");
 
 describe('Block', () => {
     const timestamp= "a time";
     const lastHash = "oldhash";
     const hash = "newhash";
-    const data = "info";
-    const block = new Block({timestamp,lastHash,hash,data});
+    const data = ["info",'data'];
+    const nonce = 1;
+    const difficulty = 1;
+    const block = new Block({timestamp,lastHash,hash,data,nonce,difficulty});
 
-    it('has timestamp,lastHash,hash and data', () => {
+    it('has timestamp,lastHash,hash,data,nonce,dfficulty', () => {
         expect(block.timestamp).toEqual(timestamp);
-        expect(block.timestamp).toEqual(timestamp);
-        expect(block.timestamp).toEqual(timestamp);
-        expect(block.timestamp).toEqual(timestamp);        
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);        
     });
 
     describe('Genesis()',()=>{
@@ -38,13 +43,22 @@ describe('Block', () => {
         it('sets lastHash to hash of lastBlock',()=>{
             expect(mineBlock.lastHash).toEqual(lastBlock.hash);
         });
+
         it('sets the data', () => {
             expect(mineBlock.data).toEqual(data);
         });
+
         it("sets time stamp",()=>{
             expect(mineBlock.timestamp).not.toEqual(undefined);
         });
-        
+
+        it('creates a sha256 hash based on valid input', () => {
+            expect(mineBlock.hash).toEqual(cryptoHash(mineBlock.timestamp,lastBlock.hash,mineBlock.difficulty,mineBlock.nonce,data));
+        });
+
+        it('sets a hash that matches the difficulty level', () => {
+            expect(mineBlock.hash.substring(0,mineBlock.difficulty)).toEqual('0'.repeat(mineBlock.difficulty));
+        });
     });
     
 });
--- config.js
+++ config.js
@@ -1,8 +1,11 @@
+const INIT_DIFFICULTY = 3;
 const GENESIS_DATA = {
     timestamp: 1,
     lastHash:"lasthash",
     hash: "hash 1",
-    data : []
+    difficulty: INIT_DIFFICULTY,
+    data : [],
+    nonce: 0,
 };
 
 module.exports = {GENESIS_DATA:GENESIS_DATA}
